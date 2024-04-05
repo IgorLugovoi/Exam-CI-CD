dfddb9105e1d15b4950b5e46ec7ffb4ed456ba8b
IgorLugovoiii <qeadzc4065@gmail.com>
2024-04-05 12:26:21 +0300

    Added method extract words, this method removes punctuation and punctuation and breaks text into words.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import re
 
 def read_text_from_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
+
+
+def extract_words(text):
+    return re.findall(r'\b\w+\b', text.lower())
 
 
 if __name__ == "__main__":
@@ -10,4 +15,5 @@ if __name__ == "__main__":
     top_n = 10
 
     text = read_text_from_file(input_filename)
+    words = extract_words(text)
 
